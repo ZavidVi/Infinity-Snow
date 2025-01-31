d7bf544ad24ff963e490fcd4fbc1d01699caf931
ZavidVi <167905222+ZavidVi@users.noreply.github.com>
2025-01-31 17:35:48 +0300

    Update InfinitySnow.java

--- src/main/java/com/zavidvi/infinitysnow/common/InfinitySnow.java
+++ src/main/java/com/zavidvi/infinitysnow/common/InfinitySnow.java
@@ -15,10 +15,8 @@ import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.levelgen.Heightmap.Types;
 
 public class InfinitySnow {
-
-
-
-    public static void chunkTick(LevelChunk chunk, int ii) {
+    
+    public static void chunkTick(LevelChunk chunk, int randomTickSpeed) {
         ServerLevel world = (ServerLevel)chunk.getLevel();
         ChunkPos chunkpos = chunk.getPos();
         boolean flag = world.isRaining();
@@ -27,11 +25,11 @@ public class InfinitySnow {
         if (world.random.nextInt(32) == 0) {
             BlockPos blockpos2 = world.getHeightmapPos(Types.MOTION_BLOCKING, world.getBlockRandomPos(i, 0, j, 15));
             Holder<Biome> biome = world.getBiome(blockpos2);
-            if (flag && shouldSnow((Biome)biome.value(), world, blockpos2)) {
+            if (flag && shouldSnow(biome.value(), world, blockpos2)) {
                 if (world.getBlockState(blockpos2).getBlock() != Blocks.GRASS && !world.getBlockState(blockpos2).is(BlockTags.FLOWERS)) {
-                    int state = (Integer)world.getBlockState(blockpos2).getValue(SnowLayerBlock.LAYERS);
+                    int state = world.getBlockState(blockpos2).getValue(SnowLayerBlock.LAYERS);
                     if (state < 8) {
-                        world.setBlockAndUpdate(blockpos2, (BlockState)Blocks.SNOW.defaultBlockState().setValue(SnowLayerBlock.LAYERS, state + 1));
+                        world.setBlockAndUpdate(blockpos2, Blocks.SNOW.defaultBlockState().setValue(SnowLayerBlock.LAYERS, state + 1));
                     }
                     else if (state == 8){
                         world.setBlockAndUpdate(blockpos2, Blocks.SNOW_BLOCK.defaultBlockState());
@@ -42,12 +40,12 @@ public class InfinitySnow {
 
     }
 
-    public static boolean shouldSnow(Biome biome, LevelReader p_201850_1_, BlockPos p_201850_2_) {
-        if (!biome.coldEnoughToSnow(p_201850_2_)) {
+    public static boolean shouldSnow(Biome biome, LevelReader levelReader, BlockPos blockPos) {
+        if (!biome.coldEnoughToSnow(blockPos)) {
             return false;
         } else {
-            if (p_201850_2_.getY() >= p_201850_1_.getMinBuildHeight() && p_201850_2_.getY() < p_201850_1_.getMaxBuildHeight() && p_201850_1_.getBrightness(LightLayer.BLOCK, p_201850_2_) < 10) {
-                BlockState blockstate = p_201850_1_.getBlockState(p_201850_2_);
+            if (blockPos.getY() >= levelReader.getMinBuildHeight() && blockPos.getY() < levelReader.getMaxBuildHeight() && levelReader.getBrightness(LightLayer.BLOCK, blockPos) < 10) {
+                BlockState blockstate = levelReader.getBlockState(blockPos);
                 if (blockstate.getBlock() == Blocks.SNOW || blockstate.getBlock() == Blocks.GRASS || blockstate.is(BlockTags.SMALL_FLOWERS)) {
                     return true;
                 }
